8880c8242ea835de81daf6ce92d442b5c82530c4
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-10-19 20:35:23 +0900

    [Test]: 이동 목표지점의 위치 선정을 위한 스크립트 추가

---
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/MovingTarget.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTarget : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+
+    // 테스트용 변수. 삭제 가능
+    [SerializeField] private int _step;
+    [SerializeField] private float _distToPlayer;
+
+    private void Update() 
+    {
+        //업데이트는 테스트용임
+    }
+
+    private void PositionSet()
+    {
+
+    }
+
+    private void RightPosition()
+    {
+
+    }
+
+    private void LeftPosition()
+    {
+
+    }
+
+}
