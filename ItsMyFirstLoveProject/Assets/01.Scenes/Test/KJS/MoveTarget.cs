0d6cfdf99f9b323c6a448c927969eef4e671c81c
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-10-22 18:15:55 +0900

    [Feat]: 플레이어 진행방향 관련 함수 로직 추가 진행중

--- ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/MoveTarget.cs
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/MoveTarget.cs
@@ -9,6 +9,8 @@ public class MoveTarget : MonoBehaviour
 
     private Vector3 _nowPosition;
     private Vector3 _prevPosition;
+    private float _nowDistance;
+    private float _prevDistance;
 
 
     private void Start() 
@@ -16,7 +18,10 @@ public class MoveTarget : MonoBehaviour
         StartCoroutine(DistanceCheck());
     }
 
-
+    private void PositionSet()
+    {
+        
+    }
 
 
     private IEnumerator DistanceCheck()
@@ -24,15 +29,25 @@ public class MoveTarget : MonoBehaviour
         while(true)
         {
             _prevPosition = _nowPosition;
+            _prevDistance = _nowDistance;
             _nowPosition = _player.transform.position;
 
             Vector3 dir = _nowPosition - _prevPosition;
-            float dist = Vector3.Distance(_nowPosition, _prevPosition);
             dir.y = 0f;
+            _nowDistance = dir.magnitude;
             Quaternion rot = Quaternion.LookRotation(dir.normalized);
 
-            //테스트용 로그출력
             Debug.Log(rot.eulerAngles);
+
+            if(_nowDistance >= 0.01)
+            {
+                transform.SetPositionAndRotation(_player.transform.position, rot);
+            }
+            else if(true)
+            {
+
+            }
+            
             yield return new WaitForSeconds(_checkCycle);
         }
     }
