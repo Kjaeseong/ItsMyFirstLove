ba88d8f480b98b31c669a8a3378ee3f20f6c9ca7
SOLP\zeeoe <solp97@naver.com>
2022-10-24 09:41:09 +0900

    [Feat] 해당 조건 만족시 챕터 오픈 기능 추가

---
+++ ItsMyFirstLoveProject/Assets/02.Scripts/OpenChapter.cs
@@ -0,0 +1,39 @@
+using Google.Maps.Examples;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenChapter : MonoBehaviour
+{
+    public bool isClear;
+    public UIManager PlayerLv;
+    public int OpenLv;
+
+    public Text ChapterText;
+    public Text NonOpenTextInfo;
+    public Image ChapterBack;
+
+    private Button _chapterButton;
+
+    private void Start()
+    {
+        _chapterButton = GetComponent<Button>();
+    }
+    private void Update()
+    {
+        InteractiveChpater();
+    }
+
+    private void InteractiveChpater()
+    {
+        if (isClear && PlayerLv.Level >= OpenLv)
+        {
+            Destroy(NonOpenTextInfo);
+            ChapterText.rectTransform.localPosition = new Vector3(0, 0, 0);
+            ChapterBack.color = new Color(255, 255, 0);
+            _chapterButton.interactable = true;
+        }
+    }
+
+}
