cb83de8a1f01b6bd33f848b5c5bd2a3fe19012a2
roomMaker <leehuk0622@gmail.com>
2022-10-24 16:18:21 +0900

    [Feat] 미니맵 줌 인& 줌 아웃 기능
    미니맵 줌 인& 줌 아웃 기능 추가

--- ItsMyFirstLoveProject/Assets/01.Scenes/Test/LJH/FullScreenMiniMap.cs
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/LJH/FullScreenMiniMap.cs
@@ -5,13 +5,51 @@ using UnityEngine;
 public class FullScreenMiniMap : MonoBehaviour
 {
     [SerializeField] GameObject _fullScreenMiniMap;
-    public void ActiveFullScreen()
+
+    [SerializeField] Camera _miniMapCamera;
+
+    private bool _isFullScreen;
+
+    private float _oldTouchDistance;
+    private float _cameraSize = 150f;
+    private float _currentTouchDistance;
+    private float _touchDistance;
+
+    private void Update()
     {
-        _fullScreenMiniMap.SetActive(true);
+        ZoomInOutMiniMap();
     }
 
-    public void InActiveFullScreen()
+    /// <summary>
+    /// 미니맵 전체화면 활성화 & 비활성화
+    /// </summary>
+    public void SwitchFullScreen()
     {
-        _fullScreenMiniMap.SetActive(false);
+        _isFullScreen = !_isFullScreen;
+        _fullScreenMiniMap.SetActive(_isFullScreen);
+    }
+
+    // 미니맵 두 손가락으로 줌 인 & 아웃 처리 함수
+    private void ZoomInOutMiniMap()
+    {
+        if (!_isFullScreen)
+        {
+            return;
+        }
+
+        if (Input.touchCount == 2 && (Input.touches[0].phase == TouchPhase.Moved || Input.touches[1].phase == TouchPhase.Moved))
+        {
+            _currentTouchDistance = (Input.touches[0].position - Input.touches[1].position).sqrMagnitude;
+
+            _touchDistance = (_currentTouchDistance - _oldTouchDistance) * 0.01f;
+
+            _cameraSize -= _touchDistance;
+
+            _cameraSize = Mathf.Clamp(_cameraSize, 70f, 300f);
+
+            _miniMapCamera.orthographicSize = Mathf.Lerp(_miniMapCamera.orthographicSize, _cameraSize, Time.deltaTime * 5f);
+
+            _oldTouchDistance = _currentTouchDistance;
+        }
     }
 }
