566fcd1d238aeca9548831aff095f2a1ce94a66e
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-10-25 15:47:08 +0900

    [Feat]: 게임매니저 스크립트 변수 및 변수 설정 함수 추가

--- ItsMyFirstLoveProject/Assets/02.Scripts/GameManager.cs
+++ ItsMyFirstLoveProject/Assets/02.Scripts/GameManager.cs
@@ -6,4 +6,29 @@ public class GameManager : SingletonBehaviour<GameManager>
 {
     private AudioManager _audio;
     private CsvManager _csv;
+    
+    public float CameraHeight { get; private set; }
+    public float MapRotateY { get; private set; }
+    public double Lat { get; private set; }
+    public double Long { get; private set; }
+
+
+
+
+    public void SetRatLong(double lat, double lng)
+    {
+        Lat = lat;
+        Long = lng;
+    }
+
+    public void SetCameraHeight(float Height)
+    {
+        CameraHeight = Height;
+    }
+
+    public void SetMapRotateY(float rotateY)
+    {
+        MapRotateY = rotateY;
+    }
+
 }
