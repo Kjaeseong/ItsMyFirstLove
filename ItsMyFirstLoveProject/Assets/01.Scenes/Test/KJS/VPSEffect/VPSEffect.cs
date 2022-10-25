4771a35cec1208acd31227dab0e60540b5f2bc64
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-10-25 12:44:47 +0900

    [Feat]: VPSEffect 전용 스크립트 및 로직 추가

---
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/VPSEffect/VPSEffect.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VPSEffect : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _firstPrefabGroup = new List<GameObject>();
+    [SerializeField] private int _firstPoolSize;
+    [SerializeField][Range(0, 30)] private float _firstPosRange;
+    [SerializeField][Range(0, 30)] private float _firstHeight;
+    [SerializeField] private List<GameObject> _secondPrefabGroup = new List<GameObject>();
+    [SerializeField] private int _secondPoolSize;
+    [SerializeField][Range(0, 30)] private float _secondPosRange;
+    [SerializeField][Range(0, 30)] private float _secondHeight;
+    [SerializeField] private List<GameObject> _thirdPrefabGroup = new List<GameObject>();
+    [SerializeField] private int _thirdPoolSize;
+    [SerializeField][Range(0, 30)] private float _thirdPosRange;
+    [SerializeField][Range(0, 30)] private float _thirdHeight;
+
+    private List<GameObject> _pool = new List<GameObject>();
+    private int _poolSize;
+    private int _formula;
+    private float _posRange;
+    private float _height;
+
+    private void Start() 
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            CreatePool(i);
+        }
+    }
+
+    private void CreatePool(int index)
+    {
+        PoolSettingCheck(index);
+
+        int count = 0;
+        for(int i = 0; i < _poolSize; i++)
+        {
+            switch(index)
+            {
+                case 0:
+                    _pool.Add(Instantiate(_firstPrefabGroup[count]));
+                    break;
+                case 1:
+                    _pool.Add(Instantiate(_secondPrefabGroup[count]));
+                    break;
+                case 2:
+                    _pool.Add(Instantiate(_thirdPrefabGroup[count]));
+                    break;
+            }
+            _pool[_pool.Count - 1].transform.parent = gameObject.transform;
+            _pool[_pool.Count - 1].transform.position = RandPos(_posRange, _height);
+            _pool[_pool.Count - 1].transform.rotation = Quaternion.Euler(0f, Random.Range(-180, 180), 0f);
+            count = (count + 1) % _formula;
+        }
+    }
+
+    private void PoolSettingCheck(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                _poolSize = _firstPoolSize;
+                _formula = _firstPrefabGroup.Count;
+                _posRange = _firstPosRange;
+                _height = _firstHeight;
+                break;
+            case 1:
+                _poolSize = _secondPoolSize;
+                _formula = _secondPrefabGroup.Count;
+                _posRange = _secondPosRange;
+                _height = _secondHeight;
+                break;
+            case 2:
+                _poolSize = _thirdPoolSize;
+                _formula = _thirdPrefabGroup.Count;
+                _posRange = _thirdPosRange;
+                _height = _thirdHeight;
+                break;
+        }
+    }
+
+    private Vector3 RandPos(float range, float posY)
+    {
+        float x = Random.Range(-1* range, range);
+        float z = Random.Range(-1* range, range);
+        Vector3 position = new Vector3(x, posY, z);
+
+        return position;
+    }
+}
--- ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/VPSEffect/VPSEffectManager.cs
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/VPSEffect/VPSEffectManager.cs
@@ -13,30 +13,53 @@ public class VPSEffectManager : MonoBehaviour
     private void Start() 
     {
         CreatePool();
+
+        // TODO : 게임매니저 제작시 사라질 코드
+        temp = 0f;
     }
 
+    /// <summary>
+    /// effectNum : 인스펙터에서 배열로 넣어 놓은 이펙트 순번, 0부터 시작함 <br/>
+    /// position : 이펙트 중심 위치
+    /// </summary>
     public void EffectSwitch(int effectNum, Vector3 position)
     {
-        _effectList[effectNum].transform.position = new Vector3(
-            position.x,
-            temp,
-            position.z
-        );
-
-        _effectList[effectNum].SetActive(true);
+        if(effectNum < _effectPool.Count)
+        {
+            _effectPool[effectNum].transform.position = new Vector3(
+                position.x,
+                // TODO : 게임매니저 제작시 아래 temp는 맵 기준높이 받아와야 함.
+                temp,
+                position.z
+            );
+            _effectPool[effectNum].SetActive(true);
+        }
+        else
+        {
+            Debug.Log("List Index Missing");
+        }
     }
 
+    /// <summary>
+    /// effectNum : 인스펙터에서 배열로 넣어 놓은 이펙트 순번, 0부터 시작함
+    /// </summary>
     public void DeactivateEffect(int effectNum)
     {
-        _effectList[effectNum].SetActive(false);
+        if(effectNum < _effectPool.Count)
+        {
+            _effectPool[effectNum].SetActive(false);
+        }
+        else
+        {
+            Debug.Log("List Index Missing");
+        }
     }
 
     private void CreatePool()
     {
         for(int i = 0; i < _effectList.Count; i++)
         {
-            _effectPool[i] = Instantiate(_effectList[i]);
-            
+            _effectPool.Add(Instantiate(_effectList[i]));
         }
     }
 }
