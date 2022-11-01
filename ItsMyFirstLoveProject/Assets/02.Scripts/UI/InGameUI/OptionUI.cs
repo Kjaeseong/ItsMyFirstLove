47977d17124d88e071fe7c9464ab809b18696b2e
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-11-01 14:16:17 +0900

    [Feat]: 환경설정 UI 임시 스크립트 작성
    추가적인 UI 기획안 수립시 추가작업 예정

--- ItsMyFirstLoveProject/Assets/02.Scripts/UI/InGameUI/OptionUI.cs
+++ ItsMyFirstLoveProject/Assets/02.Scripts/UI/InGameUI/OptionUI.cs
@@ -4,6 +4,10 @@ public class OptionUI : MonoBehaviour
 {
     private GameObject _uiObject;
 
+    /// <summary>
+    /// 함수 호출시의 UI오브젝트가 자신을 담아서 호출 <br/>
+    /// 옵션창 비활성화시 자동으로 이전 오브젝트가 활성화
+    /// </summary>
     public void ObjectSet(GameObject UI)
     {
         _uiObject = UI;
