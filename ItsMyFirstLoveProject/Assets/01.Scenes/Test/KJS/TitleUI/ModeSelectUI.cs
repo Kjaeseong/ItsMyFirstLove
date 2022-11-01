7dcebaa95489fad6511b8f985abba7783f7797d0
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-11-01 10:28:57 +0900

    [Feat]: titleUIManager 스크립트 생성

--- ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/TitleUI/ModeSelectUI.cs
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/TitleUI/ModeSelectUI.cs
@@ -10,31 +10,18 @@ public class ModeSelectUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _level;
     [SerializeField] private Image _favorability;
     [SerializeField] private GameObject _talkBox;
-    [SerializeField] private GameObject _optionUI;
-    [SerializeField] private GameObject _chapterSelectUI;
+    private TitleUIManager _title;
     private TextMeshProUGUI _talkBoxText;
     private List<string> _BoxText = new List<string>();
 
     private void Start() 
     {
+        _title = GetComponentInParent<TitleUIManager>();
         _talkBoxText = _talkBox.GetComponentInChildren<TextMeshProUGUI>();
     }
-
-    public void ActivateChapterSelect()
-    {
-        _chapterSelectUI.SetActive(true);
-        gameObject.SetActive(false);
-    }
-
     public void StartWalkMode()
     {
         GameManager.Instance._scene.Change("WalkMode");
-    }
-
-    public void ActivateOption()
-    {
-        _optionUI.SetActive(true);
-        gameObject.SetActive(false);
     }
 
     private void CharacterTouch()
@@ -42,9 +29,25 @@ public class ModeSelectUI : MonoBehaviour
         ActivateTalkBox(_BoxText[Random.Range(0, _BoxText.Count)]);
     }
 
-    private void ActivateTalkBox(string Text)
+    public void ActivateTalkBox(string Text)
     {
         _talkBoxText.text = Text;
         _talkBox.SetActive(true);
     }
+
+    public void ActivateChapterSelect()
+    {
+        _title.ActivateChapterSelect();
+    }
+    
+    public void PlayWalkMode()
+    {
+        _title.PlayWalkMode();
+    }
+
+    public void ActivateOption()
+    {
+        _title.ActivateOption();
+    }
+
 }
--- ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/TitleUI/TitleUIManager.cs
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/TitleUI/TitleUIManager.cs
@@ -6,26 +6,54 @@ public class TitleUIManager : MonoBehaviour
 {
     [SerializeField] private GameObject _modeSelectUI;
     [SerializeField] private GameObject _chapterSelectUI;
-    [SerializeField] private GameObject _stageSelectUI;
     [SerializeField] private GameObject _optionUI;
 
+
+    /// <summary>
+    /// 모드 선택창UI 출력
+    /// </summary>
     public void ActivateModeSelect()
     {
-
+        _modeSelectUI.SetActive(true);
     }
 
+    /// <summary>
+    /// 챕터 선택창UI 출력
+    /// </summary>
     public void ActivateChapterSelect()
     {
-
+        _chapterSelectUI.SetActive(true);
     }
 
-    public void ActivateStageSelect()
-    {
-
-    }
-
+    /// <summary>
+    /// 환경설정UI 출력
+    /// </summary>
     public void ActivateOption()
     {
+        _optionUI.SetActive(true);
+    }
 
+    /// <summary>
+    /// 산책모드 씬 실행
+    /// </summary>
+    public void PlayWalkMode()
+    {
+        GameManager.Instance._scene.Change("WalkMode");
+    }
+
+    /// <summary>
+    /// 스테이지모드 씬 실행
+    /// </summary>
+    public void PlayStageMode()
+    {
+        GameManager.Instance._scene.Change("StageMode");
+    }
+
+    /// <summary>
+    /// 스테이지 정보 창 활성화
+    /// </summary>
+    public void ActivateStageDescription()
+    {
+        // 혹시 모를 스테이지 시작 전 정보창을 띄우기 위한 함수.
     }
 }
