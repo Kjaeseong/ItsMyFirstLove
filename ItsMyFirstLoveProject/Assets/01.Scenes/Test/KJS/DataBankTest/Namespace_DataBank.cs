458d7593143dddf0e12a213162eadaccc526124f
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-11-08 18:04:44 +0900

    [Feat]: 데이터뱅크 스크립트 작성 진행중

---
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/KJS/DataBankTest/Namespace_DataBank.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataBank
+{
+    namespace Player
+    {
+        public class State
+        {
+            public static int Level { 
+                get => Level;
+                set => Level = value;
+            }
+            
+        }
+    }
+
+    namespace Hana
+    {
+        public class State
+        {
+
+        }
+
+    }
+}
