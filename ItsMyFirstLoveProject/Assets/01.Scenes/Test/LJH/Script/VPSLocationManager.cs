512f280d6b60f5fabc4841747e8d7605b2b39256
roomMaker <leehuk0622@gmail.com>
2022-11-08 14:48:55 +0900

    [Fix} 작업 병합 과정에서 필요한 스크립트 수정
    게임매니저 변수 추가 및 세팅 함수 추가,
    VPSLocation 관련 테스트빌드를 위해 임시 Invoke 처리

--- ItsMyFirstLoveProject/Assets/01.Scenes/Test/LJH/Script/VPSLocationManager.cs
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/LJH/Script/VPSLocationManager.cs
@@ -6,15 +6,22 @@ public class VPSLocationManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] _vpsLocation;
 
+    // 테스트용 임시 Invoke() 사용, 추후 스테이지 선택 후 ActiveVPSLocation() 실행 해야 함.
+    private void Start()
+    {
+        Invoke("ActiveVPSLocation", 3f);
+    }
+
+    // TODO : 만약 스테이지 씬 로드 전에 다른 씬에서 스테이지를 고를 경우 해당 스크립트 내용을 게임매니저에 옮기는 것을 추천
     /// <summary>
     /// 스테이지 인덱스를 통해 해당하는 VPS 이벤트 경로를 켜는 함수
     /// </summary>
     /// <param name="vpsLocationIndex">스테이지 인덱스</param>
-    public void ActiveVPSLocation(int vpsLocationIndex)
+    public void ActiveVPSLocation()
     {
         ClearVPSLocation();
 
-        _vpsLocation[vpsLocationIndex].SetActive(true);
+        _vpsLocation[GameManager.Instance.StageIndex].SetActive(true);
     }
 
     private void ClearVPSLocation()
