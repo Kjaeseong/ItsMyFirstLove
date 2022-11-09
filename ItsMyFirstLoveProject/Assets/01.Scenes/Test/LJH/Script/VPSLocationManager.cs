99d0b75ee33ef26f8ce561774b51c89258714398
RoomMaker <leehuk0622@gmail.com>
2022-11-09 14:29:57 +0900

    [Fix] VPS 로케이션 수정 및 일부 코드 테스트용 수정
    GameManager
    VPSLocationManager
    LocationEventSystem
    
    스크립트 수정

--- ItsMyFirstLoveProject/Assets/01.Scenes/Test/LJH/Script/VPSLocationManager.cs
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/LJH/Script/VPSLocationManager.cs
@@ -6,18 +6,23 @@ public class VPSLocationManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] _vpsLocation;
 
+    // TODO : 테스트를 위한 임시 Invoke()처리 했음. 추후 수정 요망
+    private void Start()
+    {
+        Invoke("ActiveVPSLocation", 3f);
+    }
     /// <summary>
     /// 스테이지 인덱스를 통해 해당하는 VPS 이벤트 경로를 켜는 함수
     /// </summary>
-    /// <param name="vpsLocationIndex">스테이지 인덱스</param>
-    public void ActiveVPSLocation(int vpsLocationIndex)
+    public void ActiveVPSLocation()
     {
-        ClearVPSLocation();
+        InActiveAllVPSLocation();
 
-        _vpsLocation[vpsLocationIndex].SetActive(true);
+        _vpsLocation[GameManager.Instance.CurrentStageIndex].SetActive(true);
     }
 
-    private void ClearVPSLocation()
+    // VPS 로케이션 전부 비활성화
+    private void InActiveAllVPSLocation()
     {
         foreach(var vpsLocation in _vpsLocation)
         {
