d7345d246bbfd8bccc283899564b7ea2381d0345
shinshinjin <sinhukjin01@naver.com>
2022-11-15 17:24:55 +0900

    [Add]: 스테이지2 버스VPS 완료

--- ItsMyFirstLoveProject/Assets/01.Scenes/Test/SHJ/BusMoveController.cs
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/SHJ/BusMoveController.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class BusMoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] _busLocation;
+    private float _speed = 5f;
+
+    private void Awake()
     {
-        
+        StartCoroutine(BusRotate());
+        StartCoroutine(BusMove());
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator BusRotate()
     {
-        
+        yield return new WaitForSeconds(10.5f);
+        while (true)
+        {
+            Debug.Log("코루틴 실행중");
+            transform.LookAt(_busLocation[1].transform);
+
+            yield return new WaitForSeconds(0.01f);
+
+            Vector3 offset = _busLocation[0].transform.position - transform.position;
+            float sqrLen = offset.sqrMagnitude;
+
+            if (sqrLen < 20f)
+            {
+                break;
+            }
+        }
+    }
+    IEnumerator BusMove()
+    {
+        while (true)
+        {
+            Vector3 offset = _busLocation[1].transform.position - transform.position;
+            float sqrLen = offset.sqrMagnitude;
+
+            transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
+            yield return new WaitForSeconds(0.005f);
+
+            if (sqrLen < 15f)
+            {
+                break;
+            }
+        }
     }
 }
