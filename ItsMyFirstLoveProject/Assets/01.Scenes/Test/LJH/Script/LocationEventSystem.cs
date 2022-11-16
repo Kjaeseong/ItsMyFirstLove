224325e075b28254aa26845893d902e4bcee3c27
RoomMaker <leehuk0622@gmail.com>
2022-11-16 10:49:43 +0900

    [Add] 스테이지 0 대사 일부 추가

--- ItsMyFirstLoveProject/Assets/01.Scenes/Test/LJH/Script/LocationEventSystem.cs
+++ ItsMyFirstLoveProject/Assets/01.Scenes/Test/LJH/Script/LocationEventSystem.cs
@@ -39,7 +39,6 @@ public class LocationEventSystem : MonoBehaviour
         public string AnimationName;
         [Header("VPSEffect")]
         public int VPSIndex;
-
     }
 
     public enum Select
